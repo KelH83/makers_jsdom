efa9ddc4d1b758bc8e5f00b785fc72b9df5da185
Kelly <kellyhowes83@gmail.com>
2024-11-18 11:49:23 +0000

    completed event listeners

--- eventListeners/index.js
+++ eventListeners/index.js
@@ -1,11 +1,35 @@
-const apiURL = "https://official-joke-api.appspot.com/random_joke";
+// 1. Getting the DOM element
+const buttonEl = document.querySelector('#my-button');
+const para = document.querySelector("p")
 
-fetch(apiURL)
-.then((response) => response.json()) 
-.then((data) => {
-    document.querySelector("h1").textContent = data.setup
-    function deliverPunchline(){
-        document.querySelector("p").textContent = data.punchline
-    }
-    const timeout = setTimeout(deliverPunchline, 2000);
+// 2. Attaching the event listener
+buttonEl.addEventListener('click', () => {
+    const randomNum = Math.floor(Math.random() * 10)
+    fetch(`https://jsonplaceholder.typicode.com/todos/${randomNum}`)
+    .then((response) => response.json())
+    .then((data) =>{
+        para.innerText = data.title
+    })
 });
+
+// // ALTERNATIVE
+
+// document.querySelector('#my-button').addEventListener('click', () => {
+//     console.log('clicked!');
+// });
+
+// // This function will attach the event listener when called 
+// const attachListener = () => {
+//     document.querySelector('#my-button').addEventListener('click', () => {
+//     console.log('clicked!');
+//     });
+// }
+
+//   // If the user clicks on the button now, nothing will happen (yet)
+
+//   // Later, we call the function defined previously
+// attachListener();
+
+//   // The click is now handled
+
+
